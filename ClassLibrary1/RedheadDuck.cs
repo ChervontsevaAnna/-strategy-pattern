372213f33a5260d55b61a8c01d9e41dee1b38adb
JollyFoxy <pavel.mizirev@gmail.com>
2022-01-21 14:07:55 +0300

    Создан абстрактный класс Duck и его наследники, реализованы методы quack, swim, display

---
+++ ClassLibrary1/RedheadDuck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SimUDuck
+{
+
+    public class ReadheaddDuck : Duck
+    {
+        public override string display()
+        {
+            return "Красно-головая " + this.GetType();
+        }
+    }
+}
